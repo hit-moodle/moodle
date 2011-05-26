b7708bd6c29f5a87d6b4f04b72495001aa4ac47e
glove <glovenone@gmail.com>
2011-05-26 09:33:43 +0800

    完成作业下载分类功能

--- mod/assignment/submissions.php
+++ mod/assignment/submissions.php
@@ -3,12 +3,10 @@
 require_once("../../config.php");
 require_once("lib.php");
 require_once($CFG->libdir.'/plagiarismlib.php');
-
 $id   = optional_param('id', 0, PARAM_INT);          // Course module ID
 $a    = optional_param('a', 0, PARAM_INT);           // Assignment ID
 $mode = optional_param('mode', 'all', PARAM_ALPHA);  // What mode are we in?
 $download = optional_param('download' , 'none', PARAM_ALPHA); //ZIP download asked for?
-
 $url = new moodle_url('/mod/assignment/submissions.php');
 if ($id) {
     if (! $cm = get_coursemodule_from_id('assignment', $id)) {
@@ -35,7 +33,6 @@ if ($id) {
     }
     $url->param('a', $a);
 }
-
 if ($mode !== 'all') {
     $url->param('mode', $mode);
 }
@@ -45,14 +42,43 @@ require_login($course->id, false, $cm);
 require_capability('mod/assignment:grade', get_context_instance(CONTEXT_MODULE, $cm->id));
 
 $PAGE->requires->js('/mod/assignment/assignment.js');
+$PAGE->requires->js('/mod/assignment/download.js');
+
 
 /// Load up the required assignment code
 require($CFG->dirroot.'/mod/assignment/type/'.$assignment->assignmenttype.'/assignment.class.php');
 $assignmentclass = 'assignment_'.$assignment->assignmenttype;
 $assignmentinstance = new $assignmentclass($cm->id, $assignment, $cm, $course);
 
-if($download == "zip") {
-    $assignmentinstance->download_submissions();
-} else {
+$go = false;
+$get_feedback = false;
+$into_folder = false;
+
+if(isset($_POST['submit_1'])){
+    $into_folder = false;
+    $go = true;   
+}
+else if(isset($_POST['submit_2'])){
+    $into_folder = true;
+    $go = true;
+}
+if(isset($_POST['feedback'])){
+    $get_feedback = true;
+    $go = true;
+}
+else{
+    $get_feedback = false;
+}
+
+if($download == "zip" && $go == true){
+    $assignmentinstance->download_submissions($get_feedback, $into_folder);
+    $download = NULL;
+    $assignmentinstance->submissions($mode);
+}
+else {
     $assignmentinstance->submissions($mode);   // Display or process the submissions
 }
+
+	
+
+
